2f45bb11cf5e178abe2643d70b2575ca65f089a1
Wedson Quintanilha da Silva <wquintanilhadasilva@gmail.com>
2022-10-18 16:18:17 -0300

    add src

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,107 @@
+package com.example.demo;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Controller
+public class GreetingController {
+	
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+	
+	@RequestMapping(value="/upload", headers=("content-type=multipart/*"), method=RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+		String  fileName = file.getOriginalFilename();
+		
+		System.out.println(fileName);
+		
+		String tmpdir = System.getProperty("java.io.tmpdir");
+        System.out.println("Temp file path: " + tmpdir);
+        
+        String tmpFile = tmpdir + "/" + fileName;
+        
+        System.out.println(tmpFile);
+        
+        try {
+			file.transferTo( new File(tmpFile));
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		
+		
+		Map<String, String> map = new HashMap<>();
+		map.put("nome", fileName);
+		map.put("tempFileName", tmpFile);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonResult = mapper.writerWithDefaultPrettyPrinter()
+		  .writeValueAsString(map);
+		
+		return new ResponseEntity<String>(jsonResult, HttpStatus.OK); 
+
+	}
+	
+	@RequestMapping(value="/delete/{file}", method=RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Void> delete(@PathVariable("file") String file) throws IOException {
+		
+		System.out.println(file);
+		
+		String tmpdir = System.getProperty("java.io.tmpdir");
+        System.out.println("Temp file path: " + tmpdir);
+        
+        String tmpFile = tmpdir + "/" + file;
+        
+        System.out.println("Deletando o arquivo " + tmpFile);
+        
+        Files.delete(Path.of(tmpFile));
+        
+		return new ResponseEntity<Void>(HttpStatus.OK); // ("File uploaded successfully.");
+
+	}
+	
+	@RequestMapping(value="/save", consumes = "application/json", method=RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<String> saveForm(@RequestBody LinkedHashMap<String,Object> formulario) throws IOException {
+		
+		System.out.println(formulario);
+		
+		Map<String, String> map = new HashMap<>();
+		map.put("mensagem", "Dados gravados com sucesso!");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonResult = mapper.writerWithDefaultPrettyPrinter()
+		  .writeValueAsString(map);
+		
+		return new ResponseEntity<String>(jsonResult, HttpStatus.OK);
+
+	}
+
+	
+}
---
+++ src/main/webapp/js/jquery-lab.js
@@ -0,0 +1,115 @@
+$(function() {
+
+    var files = [];
+	var rowIdx = 0;
+	
+	$("#faleConoscoForm").on("submit", function(event){
+		
+		event.preventDefault();
+		
+        var data = $('#faleConoscoForm').serializeJSON();
+		data.arquivos = files;
+        console.log(data);
+    	var jsonData = JSON.stringify(data);
+		console.log(jsonData);
+        $.ajax({
+            url: '/save',
+            data: jsonData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            method: 'POST',
+            type: 'POST',
+			dataType : 'json',
+			contentType: 'application/json',
+    
+            success:function(data){
+                console.log(data);
+				files = [];
+				tableRepaint();
+				$('#faleConoscoForm').trigger("reset");
+            },
+            error: function(jqxhr, textStatus, errThrown){
+                console.log(textStatus);
+                console.log(errThrown);
+            }
+        });
+    });
+
+    $("#fileLoader").on("change", function(){
+
+        var formTag = $("#import-form")[0];
+        var formData = new FormData(formTag);
+        formData.append("file", $(this)[0].files[0]);
+    
+        $.ajax({
+            url: '/upload',
+            data:formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            method: 'POST',
+            type: 'POST',
+    
+            success:function(data){
+                console.log(data);
+				let result = JSON.parse(data);
+				console.log(result);
+				files.push(result);
+				tableAddRow(result);
+            },
+            error: function(jqxhr, textStatus, errThrown){
+                console.log(textStatus);
+                console.log(errThrown);
+            }
+        });
+    });
+
+
+	$('#tbody').on('click', '.remove', function () {
+		
+		// Getting <tr> id.
+        var id = $(this).attr('id');
+
+	    var fileToRemove = files[id];
+	    console.log(fileToRemove);
+
+		$.ajax({
+            url: `/delete/${fileToRemove.nome}`,
+            method: 'DELETE',
+            type: 'DELETE',
+    
+            success:function(data){
+                console.log(data);
+				files.splice(id,1);
+				tableRepaint();
+
+            },
+            error: function(jqxhr, textStatus, errThrown){
+                console.log(textStatus);
+                console.log(errThrown);
+            }
+        });
+  
+    });
+
+	function tableAddRow(data) {
+		$('#tbody').append(`
+		<tr>
+          <td class="row-index text-center">
+              <p>${data.nome}</p></td>
+       	  <td class="text-center">
+              <button id="${rowIdx++}" class="btn btn-danger remove" type="button">Remover</button>
+          </td>
+        </tr>`);
+	}
+	
+	function tableRepaint() {
+		$('#tbody').empty();
+		rowIdx = 0;
+		files.forEach(item => {
+			tableAddRow(item);
+		});
+	}
+
+});
